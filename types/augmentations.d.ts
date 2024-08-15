db3c8c0a5dd363e5eec77452704996b7cf9518be
Alexander Petree <apetree1001@email.phoenix.edu>
2024-08-15 05:09:58 -0700

    Update augmentations.d.ts

--- types/augmentations.d.ts
+++ types/augmentations.d.ts
@@ -1,10 +1,12 @@
-import { Accounts, Signers } from "./";
-import { Greeter } from "../typechain/Greeter";
-
-declare module "mocha" {
-  export interface Context {
-    accounts: Accounts;
-    greeter: Greeter;
-    signers: Signers;
-  }
-}
+import
+  { Accounts, Signers } 
+  from "./"; import 
+  { Greeter } from 
+    "../typechain/Greeter";
+declare module
+  "mocha" {export
+  interface Context { accounts: 
+                     Accounts; greeter: 
+                     Greeter;signers: 
+                     Signers;}
+          }
