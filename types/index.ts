1d135813557526715ea15cbdeb8dfdd31cd3a1e2
Alexander Petree <apetree1001@email.phoenix.edu>
2024-08-15 05:12:08 -0700

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,17 +1,13 @@
-import { Signer } from "@ethersproject/abstract-signer";
-
-export interface Accounts {
-  admin: string;
-  alice: string;
-  bob: string;
-  eric: string;
-  lester: string;
-}
-
-export interface Signers {
-  admin: Signer;
-  alice: Signer;
-  bob: Signer;
-  eric: Signer;
-  lester: Signer;
-}
+import 
+{ Signer } from
+  "@ethersproject/abstract-signer";export
+    interface  Accounts
+{admin:
+      string;alice:
+string; bob: string; eric: string;
+lester: string;} export 
+    interface Signers {admin: Signer;alice: 
+                       Signer; bob: Signer;
+                       eric: Signer;lester: 
+                       Signer;
+                      }
